e2c34af9331939f2b49b4be83d47fc123c32f369
isMack <87532705+19SunRiser@users.noreply.github.com>
2024-03-12 04:08:25 +0000

    veriables and type conversion

---
+++ javascript_01/typeConversion.js
@@ -0,0 +1,9 @@
+let name = "hitesh";
+let n =Number(name);
+console.log(n);
+console.log(typeof n)
+
+// practice pending
+// Boolean
+// Number
+// String
---
+++ javascript_01/veriables.js
@@ -0,0 +1,14 @@
+"use strict"
+
+var id = 1122;
+let pass= "jjskdjflksdfjow";
+const UAN = 2343422;
+
+
+
+pass = "my is doller"
+ id ="changed";
+ 
+ let name = null;
+ 
+ console.table([id,pass,UAN,name]);
